51c4e134346f86610a56dff4824506e19b424f91
Antonio Martínez <toni.am.martinez@gmail.com>
2022-04-18 11:12:46 +0200

    Innit

---
+++ README.md
@@ -0,0 +1,30 @@
+# fizzbuzz
+
+- Write a program that prints one line for each number from 1 to 100
+- For multiples of three print Fizz instead of the number
+- For the multiples of five print Buzz instead of the number
+- For numbers which are multiples of both three and five print FizzBuzz instead of the number
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Test
+
+```sh
+npm run test
+```
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// Write a program that prints one line for each number from 1 to 100
+// For multiples of three print Fizz instead of the number
+// For the multiples of five print Buzz instead of the number
+// For numbers which are multiples of both three and five print FizzBuzz instead of the number
+</script>
+
+<template>
+  <ul>
+    <li>Write a program that prints one line for each number from 1 to 100</li>
+    <li>For multiples of three print Fizz instead of the number</li>
+    <li>For the multiples of five print Buzz instead of the number</li>
+    <li>
+      For numbers which are multiples of both three and five print FizzBuzz
+      instead of the number
+    </li>
+  </ul>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
